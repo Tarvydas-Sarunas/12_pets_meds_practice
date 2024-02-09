797ce2b523f43ff6b4f086cfa55fb227d8fce7c5
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-09 20:49:45 +0100

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div className='container mx-auto'>
+    <div className='mx-auto'>
+      <Header />
     </div>
   );
 }
